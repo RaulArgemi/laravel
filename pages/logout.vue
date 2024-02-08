3cbba8b726f6d0820edef92ba5dc60ada8ae699e
Raul <raul.argemi.7e6@itb.cat>
2024-02-08 19:08:21 +0100

    commit

---
+++ pages/logout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Logging Out...</div>
+</template>
+
+<script setup>
+import axios from "axios";
+
+const logout = async () => {
+  await axios.post("/logout");
+  useRouter().replace("/login");
+};
+
+logout();
+</script>
---
+++ pages/me.vue
@@ -0,0 +1,27 @@
+<template>
+  <p><strong>User Name:</strong> {{ name }}</p>
+  <p><strong>User Email:</strong> {{ email }}</p>
+</template>
+
+<script setup>
+import axios from "axios";
+
+const name = ref("");
+const email = ref("");
+
+const getUserInfo = async () => {
+  let resUserInfo;
+  try {
+    resUserInfo = await axios.get("/user");
+    console.log(resUserInfo.data);
+    name.value = resUserInfo.data?.name;
+    email.value = resUserInfo.data?.email;
+    console.log(name.value, email.value);
+  } catch (e) {
+    console.log(`Ha ocurrido un error: ${e}`);
+  }
+  console.log(resUserInfo);
+};
+
+getUserInfo();
+</script>
